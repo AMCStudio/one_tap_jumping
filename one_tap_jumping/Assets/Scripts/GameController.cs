0f54a5d7cbae57b423aa10c271265aeefd46770c
Adrià Martín <elsuperchowchow@gmail.com>
2016-06-16 23:52:07 +0200

    movement and score

--- one_tap_jumping/Assets/Scripts/GameController.cs
+++ one_tap_jumping/Assets/Scripts/GameController.cs
@@ -2,63 +2,75 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class GameController : MonoBehaviour {
 
     public GameObject new_terrain;
+    public GameObject player;
     public List<GameObject> blocks;
+    private GameObject first_terrain;
 
-    private GameObject player;
-    private enum player_change_pos
-    {
-        left,
-        right,
-    };
-    player_change_pos player_direction;
-
+    
     public Color color1 = Color.red;
     public Color color2 = Color.blue;
-    public float duration = 3.0F;
+    public float duration = 5.0F;
 
     Camera camera;
 
     public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
 
+    float x_offset = 0.50f;
+    float y_offset = 0.25f;
+    float z_offset = 0.1f;
+
 	// Use this for initialization
 	void Start () {
         blocks = new List<GameObject>();
-        InvokeRepeating("create_blocks", 0.20f, 0.55f);
-        InvokeRepeating("move_player", 0.55f, 0.55f);
-        InvokeRepeating("remove_blocks", 1.5f, 0.75f);
-        GameObject terrain_clone = (GameObject)Instantiate(new_terrain, new Vector3(0, 0, 0), Quaternion.identity);
-        terrain_clone.tag = "right";
-        blocks.Add(terrain_clone);
-        player = GameObject.FindGameObjectWithTag("Player");
-        player_direction = player_change_pos.right;
+        InvokeRepeating("create_first_blocks", 0, 0.5f);
         camera = GetComponent<Camera>();
         camera.clearFlags = CameraClearFlags.SolidColor;
+        first_terrain = GameObject.FindGameObjectWithTag("first");
+        blocks.Add(first_terrain);
 	}
 
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            if(player_direction == player_change_pos.right){
-                player_direction = player_change_pos.left;
-            }
-            else
-            {
-                player_direction = player_change_pos.right;
-            }
-        }
-
+        //Change background color
         float t = Mathf.PingPong(Time.time, duration) / duration;
         camera.backgroundColor = Color.Lerp(color1, color2, t);
 
-        Vector3 point = camera.WorldToViewportPoint(player.transform.position);
-        Vector3 delta = player.transform.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-        Vector3 destination = transform.position + delta;
-        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        //Camera smooth follow player
+        if (!player.GetComponent<player>().dead)
+        {
+            Vector3 point = camera.WorldToViewportPoint(player.transform.position);
+            Vector3 delta = player.transform.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.25f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+        else
+        {
+            CancelInvoke();
+        }
+    }
+
+    void create_first_blocks()
+    {
+        
+        GameObject terrain_clone = (GameObject)Instantiate(new_terrain, new Vector3(x_offset, y_offset, z_offset), Quaternion.identity);
+        terrain_clone.tag = "right";
+        blocks.Add(terrain_clone);
+        
+        x_offset += 0.5f;
+        y_offset += 0.25f;
+        z_offset += 0.1f;
+
+        if (blocks.Count == 3)
+        {
+            InvokeRepeating("create_blocks", 0.50f, 0.55f);
+            InvokeRepeating("remove_blocks", 2.5f, 0.55f);
+            CancelInvoke("create_first_blocks");
+        }
     }
 	
 	void create_blocks(){
@@ -66,16 +78,18 @@ public class GameController : MonoBehaviour {
         
         float choose_direction = Random.Range(0f, 100f);
 
-        if (choose_direction < 10f)
+        if (choose_direction < 20f)
         {
             if (blocks[blocks.Count - 1].tag == "right")
             {
+                blocks[blocks.Count - 1].tag = "left";
                 GameObject terrain_clone = (GameObject)Instantiate(new_terrain, new Vector3(last_item_pos.x - 0.50f, last_item_pos.y + 0.25f, last_item_pos.z + 0.01f), Quaternion.identity);
                 terrain_clone.tag = "left";
                 blocks.Add(terrain_clone);
             }
             else
             {
+                blocks[blocks.Count - 1].tag = "right";
                 GameObject terrain_clone = (GameObject)Instantiate(new_terrain, new Vector3(last_item_pos.x + 0.50f, last_item_pos.y + 0.25f, last_item_pos.z + 0.01f), Quaternion.identity);
                 terrain_clone.tag = "right";
                 blocks.Add(terrain_clone);
@@ -104,16 +118,4 @@ public class GameController : MonoBehaviour {
         blocks.Remove(blocks[0]);
     }
 
-    void move_player()
-    {
-        if (player_direction == player_change_pos.right)
-        {
-            player.transform.position = new Vector3(player.transform.position.x + 0.50f, player.transform.position.y + 0.25f, player.transform.position.z);
-        }
-        else
-        {
-            player.transform.position = new Vector3(player.transform.position.x - 0.50f, player.transform.position.y + 0.25f, player.transform.position.z);
-        }
-        
-    }
 }
---
+++ one_tap_jumping/Assets/Scripts/player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+    private enum player_change_pos
+    {
+        left,
+        right,
+    };
+    player_change_pos player_direction;
+
+    private Rigidbody2D player_body;
+    public bool dead = false;
+
+    public GameObject score_text;
+    private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+        player_direction = player_change_pos.right;
+        InvokeRepeating("move_player", 2.0f, 0.55f);
+        player_body = GetComponent<Rigidbody2D>();
+        score_text = GameObject.FindGameObjectWithTag("score");
+        score_text.GetComponent<Text>().text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //change player direction when pressing space
+        if (Input.GetKeyDown("space"))
+        {
+            if (player_direction == player_change_pos.right)
+            {
+                player_direction = player_change_pos.left;
+            }
+            else
+            {
+                player_direction = player_change_pos.right;
+            }
+        }
+	}
+
+    void move_player()
+    {
+        if (player_direction == player_change_pos.right)
+        {
+            transform.position = new Vector3(transform.position.x + 0.50f, transform.position.y + 0.25f, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x - 0.50f, transform.position.y + 0.25f, transform.position.z);
+        }
+        score += 1;
+        score_text.GetComponent<Text>().text = score.ToString();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "left" && player_direction == player_change_pos.right ||
+            other.gameObject.tag == "right" && player_direction == player_change_pos.left)
+        {
+            CancelInvoke();
+            player_body.gravityScale = 1;
+            dead = true;
+        }
+    }
+}
